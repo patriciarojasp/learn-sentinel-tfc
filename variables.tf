ef3b73bc369974e5c9a6e5a709d69d69ac49e04d
patriciarojasp <105741288+patriciarojasp@users.noreply.github.com>
2025-01-13 13:52:29 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default = "t2.micro"
+  default = "t2.small"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default = "Provisioned by HCP Terraform"
+  default = "Provisioned by Terraform"
 }

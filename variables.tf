dfc44b6f585b840067bfd65835c59730a8c2969f
patriciarojasp <105741288+patriciarojasp@users.noreply.github.com>
2025-01-16 15:14:44 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default = "t2.small"
+  default = "t2.xlarge"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default = "Provisioned by Terraform"
+  default = "Provisioned by Terraform by Patricia"
 }
